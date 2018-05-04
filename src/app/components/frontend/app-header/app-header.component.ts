057497f4eb8330fb4a6f8a197a2439a1e613dd59
Tuan <tuannd@gemvietnam.com>
2018-05-04 17:30:55 +0700

    header

--- src/app/components/frontend/app-header/app-header.component.ts
+++ src/app/components/frontend/app-header/app-header.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './app-header.component.html',
+    selector: 'app-header',
+    templateUrl: './app-header.component.html',
+    styleUrls: ['./app-header.component.scss']
 })
 export class AppHeaderComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
