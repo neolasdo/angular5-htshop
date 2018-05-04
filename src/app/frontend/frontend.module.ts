6f015a9c6796cddfc1cb29a7c916b6caeb0b8917
Tuan <tuannd@gemvietnam.com>
2018-05-04 18:23:01 +0700

    header & footer

--- src/app/frontend/frontend.module.ts
+++ src/app/frontend/frontend.module.ts
@@ -6,8 +6,10 @@ import { HomeComponent } from './home/home.component';
 import {FrontendLayoutComponent} from '../layouts/frontend-layout';
 // app components
 import { AppHeaderComponent} from '../components/frontend/';
+import {AppFooterComponent} from '../components/frontend/app-footer';
 const APP_COMPONENTS = [
-    AppHeaderComponent
+    AppHeaderComponent,
+    AppFooterComponent
 ]
 @NgModule({
   imports: [
