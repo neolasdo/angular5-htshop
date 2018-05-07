a23cb545c91a9346d013cd8bf6f4d2258b2c3a12
Tuan <tuannd@gemvietnam.com>
2018-05-07 09:11:08 +0700

    nav

--- src/app/frontend/frontend.module.ts
+++ src/app/frontend/frontend.module.ts
@@ -1,25 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { FrontendRoutingModule } from './frontend-routing.module';
-import { HomeComponent } from './home/home.component';
+import {FrontendRoutingModule} from './frontend-routing.module';
+import {HomeComponent} from './home/home.component';
 import {FrontendLayoutComponent} from '../layouts/frontend-layout';
 // app components
-import { AppHeaderComponent} from '../components/frontend/';
-import {AppFooterComponent} from '../components/frontend/app-footer';
+import {AppHeaderComponent, AppFooterComponent, AppNavbarComponent} from '../components/frontend/';
+import {BsDropdownModule, CollapseModule} from 'ngx-bootstrap';
+
 const APP_COMPONENTS = [
     AppHeaderComponent,
-    AppFooterComponent
+    AppFooterComponent,
+    AppNavbarComponent
 ]
+
 @NgModule({
-  imports: [
-    CommonModule,
-    FrontendRoutingModule
-  ],
-  declarations: [
-      ...APP_COMPONENTS,
-      HomeComponent,
-      FrontendLayoutComponent
-  ]
+    imports: [
+        CommonModule,
+        FrontendRoutingModule,
+        CollapseModule.forRoot(),
+        BsDropdownModule.forRoot()
+    ],
+    declarations: [
+        ...APP_COMPONENTS,
+        HomeComponent,
+        FrontendLayoutComponent
+    ]
 })
-export class FrontendModule { }
+export class FrontendModule {
+}
